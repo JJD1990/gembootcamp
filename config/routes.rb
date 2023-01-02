be358196cda1982e69e4d598a243d0fe237e6ef0
jjd1990 <jack.douglas@rocketmail.com>
2023-01-02 12:50:47 +0000

    devise trackable added, missed confirmable as not needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   root 'home#index'
   # get 'static_pages/privacy_policy'
   get 'privacy_policy', to: 'home#privacy_policy'
