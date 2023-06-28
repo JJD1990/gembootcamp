fb3eee440520accaba2eca152d99eba0ded89e38
jack douglas <jackdouglas@jacks-MacBook-Pro-1409.local>
2023-06-28 12:56:16 +0100

    & added to course policy to fix signing out bug

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -7,11 +7,11 @@ class CoursePolicy < ApplicationPolicy
   end
 
   def edit?
-    @user.has_role? :admin || @record.user = @user
+    @user&.has_role? :admin || @record.user = @user
   end  
 
   def update?
-    @user.has_role? :admin || @record.user = @user
+    @user&.has_role? :admin || @record.user = @user
   end  
 
   def new?
