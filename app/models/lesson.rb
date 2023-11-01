57ef63f8f48404e993fca7ceb2fa47c22b0e3d2d
JJD1990 <jack.douglas@rocketmail.com>
2023-11-01 21:37:50 +0000

    video and thumbnail functionality added to lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,8 +5,16 @@ class Lesson < ApplicationRecord
   has_many :user_lessons, dependent: :destroy
   # Course.find_each { |course| Course.reset_counters(course.id, :lessons) } to reset cache_counter
   validates :title, :content, :course, presence: true
-  validates :title, uniqueness: true, length: { :maximum => 50 }
-
+  validates :title, length: { :maximum => 50 }
+  validates_uniqueness_of :title, scope: :course_id
+  has_one_attached :video
+  has_one_attached :video_thumbnail
+  validates :video,
+    content_type: ['video/mp4'], 
+    size: { less_than: 50.megabytes , message: 'size should be under 50 megabytes' }
+  validates :video_thumbnail,
+    content_type: ['image/png', 'image/jpg', 'image/jpeg'], 
+    size: { less_than: 500.kilobytes , message: 'size should be under 500 kilobytes' }
 
   has_rich_text :content
 
