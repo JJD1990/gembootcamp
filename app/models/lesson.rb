dc21ae4bab26d3b7bd78475986c532da1e1e28ec
JJD1990 <jack.douglas@rocketmail.com>
2023-11-14 21:34:54 +0000

    lesson conditionals added

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -16,6 +16,11 @@ class Lesson < ApplicationRecord
     content_type: ['image/png', 'image/jpg', 'image/jpeg'], 
     size: { less_than: 500.kilobytes , message: 'size should be under 500 kilobytes' }
 
+    validates :video_thumbnail, presence: true, if: :video_present?
+    def video_present?
+      self.video.present?
+    end
+
   has_rich_text :content
 
   extend FriendlyId
